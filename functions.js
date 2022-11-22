3a3cea42d29b64bf2ba92a548252e3d519017412
CaferAYDOGAN <cfrayd19@gmail.com>
2022-11-22 16:18:18 +0100

    ödev yapildi

--- functions.js
+++ functions.js
@@ -1,10 +1,24 @@
-const findTheNumberOfOddNumbers = () => {};
+const findTheNumberOfOddNumbers = (pMyArray) => {
+  const oddArray = pMyArray.filter((item) => item % 2 === 1);
+  return oddArray.length;
+};
 
-const getIndexOfFalseItem = () => {};
+const getIndexOfFalseItem = (pMyArray) => {
+  const myItemIndex = pMyArray.findIndex((item) => item === false);
+  return myItemIndex;
+};
 
-const filterNumbers = () => {};
+const filterNumbers = (pMyArray) => {
+  const numberArray = pMyArray.filter((item) => typeof item == "number");
+  return numberArray;
+};
 
-const generateEmailAddress = () => {};
+const generateEmailAddress = (pMyArray) => {
+  const eMailAdresse = pMyArray.map((item) =>
+    item.toLowerCase().concat("@gmail.com")
+  );
+  return eMailAdresse;
+};
 
 export {
   findTheNumberOfOddNumbers,
